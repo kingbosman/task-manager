d2668e3c8048d6a9fe9eff5a7ad092813a80e346
King Bosman <r.bosman.zakelijk@gmail.com>
2025-09-03 12:03:31 +0200

    sqlite implementation

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,24 @@
 package cmd
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
+var db *sql.DB
+
 var rootCmd = &cobra.Command{
 	Use:   "task-manager",
 	Short: "task-manager is a simple task manager for keeping track of tasks",
 	Long: `A simple yet useable taskmanager. its implicity will make it work keeping track of pending tasks
 	the CLI is completely written in golang`,
+}
+
+func SetDatabase(conn *sql.DB) {
+	db = conn
 }
 
 func Execute() {
--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+
 	"github.com/kingbosman/task-manager/cmd"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3", "./taskmanager.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	log.Println("Connected to sqllite3")
+	// TODO: create table if not exist
+	cmd.SetDatabase(db)
 	cmd.Execute()
 }
